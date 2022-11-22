3e4348399ef80cb5011fd9534d5f8e09de89485c
JsIqbal <javascriptiqbal@gmail.com>
2022-11-22 18:30:01 +0600

    Resolve Conflicts

--- src/components/common/table.component.js
+++ src/components/common/table.component.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import TableHeader from './table.header.component';
 import TableBody from './table.body.component';
 
@@ -7,13 +6,37 @@ import TableBody from './table.body.component';
 */
 
 function Table(props) {
-    const { items, columns } = props;
+    const { items, columns, currentPage, limit } = props;
+    const start = ((currentPage - 1) * limit) + 1;
+    const end = currentPage * limit;
+    const total = items.length;
+    const pageCount = total/limit;
+    const pages = [];
+    for(let i = 1; i<= pageCount ; i++) {
+        pages.push(i);
+    }
+
+    const filteredItems = items.filter((item, index) => index+1 >= start && index+1 <= end );
 
     return (
-        <table className="table">
-            <TableHeader columns={columns} />
-            <TableBody items={items} columns={columns} />
-        </table>
+        <>
+            <table className="table">
+                <TableHeader columns={columns} />
+                <TableBody items={filteredItems} columns={columns} />
+            </table>
+            
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item"><a class="page-link">Previous</a></li>
+                    {
+                        pages.map((page) => (
+                            <li className={currentPage === page ? `page-item active` : "page-item" }><a class="page-link">{page}</a></li>
+                        ))
+                    }
+                    <li class="page-item"><a class="page-link">Next</a></li>
+                </ul>
+            </nav>
+        </>
     );
 }
  
